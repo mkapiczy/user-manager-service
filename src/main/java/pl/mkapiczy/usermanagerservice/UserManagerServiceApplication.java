1cfb0f18a12e75122ddcab2acff480e73cc0ae6c
nh51955-hertz-net <michal.kapiczynski@hertz.com>
2020-03-27 20:16:45 +0100

    Initiate project

---
+++ src/main/java/pl/mkapiczy/usermanagerservice/UserManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mkapiczy.usermanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mkapiczy/usermanagerservice/UserManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkapiczy.usermanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
